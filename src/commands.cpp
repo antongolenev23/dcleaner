8b41691b49c9cdad217d1548f4bec570a721f2db
antongolenev <antongolenev23@mail.ru>
2025-08-16 20:34:20 +0300

    fix: fix bug with delete with no parameters

--- src/commands.cpp
+++ src/commands.cpp
@@ -219,21 +219,9 @@ void Analyze::add_file_to_deletion_list(const fs::path& file) {
   deletion_list_file_ << fs::absolute(file).native() << '\n';
 }
 
-Delete::Delete(Logger& logger, const UserParameters& parameters) : Command{logger}, parameters_(parameters) {
-  deletion_list_file_.open(DELETION_LIST_FILE, std::ios::in);
-  if (!deletion_list_file_) {
-    LOG_ERROR(logger_, "cannot open deletion list file");
-    throw std::runtime_error("failed to open deletion list");
-  }
-}
+Delete::Delete(Logger& logger, const UserParameters& parameters) : Command{logger}, parameters_(parameters) {}
 
-Delete::Delete(Logger& logger, UserParameters&& parameters) : Command{logger}, parameters_(std::move(parameters)) {
-  deletion_list_file_.open(DELETION_LIST_FILE, std::ios::in);
-  if (!deletion_list_file_) {
-    LOG_ERROR(logger_, "cannot open deletion list file");
-    throw std::runtime_error("failed to open deletion list");
-  }
-}
+Delete::Delete(Logger& logger, UserParameters&& parameters) : Command{logger}, parameters_(std::move(parameters)) {}
 
 void Delete::delete_permanently(DeleteOutput& output) {
   std::string current_path;
